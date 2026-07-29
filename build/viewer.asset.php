2b75c5859af73efb9370035653168bb6248d3fb3
pcmoreno <pcmoreno@yahoo.com>
2026-07-29 18:34:17 +0200

    Build frontend
    build/ is committed because SiteGround has no Node, so the compiled bundle
    ships with the git pull.

--- build/viewer.asset.php
+++ build/viewer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '11dd4cfa151bdd72f293');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '52facaa14a542e0cdba8');
